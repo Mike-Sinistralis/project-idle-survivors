b3489a4f23685d4f1555a05ba75631dc5c81d196
Michael Zimmerman <mzimmerman0205@gmail.com>
2024-03-27 17:26:37 -0400

    Added this as a migration just to demonstrate.

--- server/src/db/migrations/2024-03-27 Implement User Sessions.sql
+++ server/src/db/migrations/2024-03-27 Implement User Sessions.sql
@@ -2,7 +2,7 @@ DO $$
 DECLARE
   migration_name VARCHAR(255);
 BEGIN
-  migration_name := -- TODO: 'yyyy-mm-dd [Description of what the script does]';
+  migration_name := '2024-03-27 Implement User Sessions'
 
 	-- (LEAVE THIS AS-IS) Check if this migration has already been applied
   
@@ -11,14 +11,23 @@ BEGIN
 		RETURN;
 	END IF;
 
-	-- 
-  -- MIGRATION START
-	-- 
+  CREATE TABLE IF NOT EXISTS "user-session" (
+    "sid" varchar NOT NULL COLLATE "default",
+    "sess" json NOT NULL,
+    "expire" timestamp(6) NOT NULL
+  ) WITH (OIDS=FALSE)
+
+  TABLESPACE pg_default;
+
+  ALTER TABLE "user-session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+  
+  ALTER TABLE IF EXISTS public."user"
+    OWNER to postgres;
 
   /*
    * How to create a DB migration script:
    * 1. Make a copy of this file and rename it to: 
-   *    yyyy-mm-dd [Description of what the script does].sql
+   *    yyyy-mm-dd [Description of what the script does] BT-XXXX.sql
    *
    * 2. Set the migration_name variable to the name of the migration. 
    *    This is used to check if the migration has already been applied.
