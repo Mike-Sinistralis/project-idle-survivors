5901861846d92580150493f009e29ad20aa7afbc
Michael Zimmerman <mzimmerman0205@gmail.com>
2024-03-27 00:08:14 -0400

    Setup typescript support so we can make sense of ldtk data, and inject it into the window so we can start to decipher how to use it.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Providers from 'root/Providers';
 import CssBaseline from '@mui/material/CssBaseline';
+import { World as WorldLoader } from 'ldtk';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,3 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Providers />
   </React.StrictMode>,
 );
+
+WorldLoader.loadRaw('./src/assets/tilemaps/ldtk/dungeon.ldtk').then(async (world) => {
+  // You have access to the raw `LDtk` JSON file here
+  (window as any).world = world;
+  world.levels[0]
+});
